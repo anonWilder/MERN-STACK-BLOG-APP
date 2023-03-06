a15fa4bd033472917b4cb09b4e96d3a012777453
anonWilder <austineokafor.e@gmail.com>
2023-03-06 02:36:20 +0100

    writing code for the blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href='' className='logo'>MyBlog</a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
       </header>
-    </div>
+      <div className='entry'>
+        <img src='https://miro.medium.com/v2/resize:fit:640/format:webp/1*QJnvahq_EBdUGjYQUYrhvA.png' alt=''></img>
+        <h2>
+          Get Started with MongoDB: Quick and Easy Installation Guide for The Latest version (6.0.4)
+        </h2>
+        <p>MongoDB, otherwise known as MongoDataBase is a popular and powerful open-source NoSQL database developed by a company called MongoDB, Inc., which was founded in 2007 by Dwight Merriman, Eliot Horowitz, and Kevin Ryan.</p>
+      </div>
+    </main>
   );
 }
 
