914fb8cdef2d6badc224e9b804db740ecfd3511e
anonWilder <austineokafor.e@gmail.com>
2023-03-07 16:26:59 +0100

    Finally imported all the importables that showed me shege

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import './App.css';
 import Post from "./Post";
 import Header from "./Header";
 import { Route, Routes } from 'react-router-dom';
+import Layout from "./Layout";
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Routes>
-      <Route index element={
-        <main>
-          <Header />
-          <Post />
-          <Post />
-          <Post />
-        </main>
-      } />
-      <Route path={'/login'} element={
-        <div>Login</div>
-      } />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element={<LoginPage />} />
+      </Route>
     </Routes>
-
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function Header(){
     return(
         <header>
-        <a href='' className='logo'>MyBlog</a>
+        <Link to='' className='logo'>MyBlog</Link>
         <nav>
-          <Link href='/login'>Login</Link>
-          <Link href='/register'>Register</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/register'>Register</Link>
         </nav>
       </header>
     );
